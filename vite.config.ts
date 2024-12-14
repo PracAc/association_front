22829c983ccf88cb21ed0621a67d8f26f9bd61a7
minjae <wlstod98@gmail.com>
2024-12-15 02:08:08 +0900

    git action test 1

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // 글로벌 함수 사용 가능
+    environment: 'jsdom', // React 테스트에 필요한 jsdom 환경
+    coverage: {
+      provider: 'istanbul', // 코드 커버리지 설정
+    },
+  },
 })
