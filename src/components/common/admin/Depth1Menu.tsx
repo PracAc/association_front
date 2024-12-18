46f429beefcb2aedb6263d1619af89836914ecce
MinJae Kim <skwze@naver.com>
2024-12-18 10:58:16 +0900

    KMJ 메뉴 이미지 깨짐 수정 (2024-12-18)

--- src/components/common/admin/Depth1Menu.tsx
+++ src/components/common/admin/Depth1Menu.tsx
@@ -43,7 +43,7 @@ function Depth1Menu({ mainName, subMenus, basicPath, iconName }: Depth1MenuProps
     const [isToggle, setIsToggle] = useState(false);
 
     // const iconPath = `/src/assets/img/icons/${iconName}`;
-    const iconPath = `/assets/img/icons/${iconName}`;
+    const iconPath = `https://www.busosi.com/assets/img/icons/${iconName}`;
 
 
     return (
