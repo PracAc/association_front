a73e2e4f5dde05bb1a25d81fc9d9e92960d92db5
MinJae Kim <skwze@naver.com>
2024-12-19 18:16:57 +0900

    KMJ 협회 메인 화면 문구 반응형 처리(2024-12-18)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import UserHeaderComponent from "../components/common/user/UserHeaderComponent.tsx";
 import UserMainFooterComponent from "../components/common/user/UserMainFooterComponent.tsx";
-import video from "../assets/video/video_text.mp4";
+import video from "../assets/video/video.mp4";
 
 
 function MainLayout({children}: { children: React.ReactNode }) {
@@ -19,6 +19,15 @@ function MainLayout({children}: { children: React.ReactNode }) {
                     <source src={video}/>
                     Your browser does not support the video tag.
                 </video>
+
+                {/* 비디오에 텍스트 오버레이 추가 */}
+                <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+                    {/* 텍스트와 배경 처리 */}
+                    <div className="text-white text-center text-3xl md:text-5xl lg:text-7xl xl:text-8xl font-bold leading-8 p-6 bg-gray-700 bg-opacity-30 rounded-md">
+                        <p>자신만의 특별한 작품을</p>
+                        <p>BLAA에서 인증받아보세요</p>
+                    </div>
+                </div>
 
                 {children}
             </main>
