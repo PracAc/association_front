580822dfaf0327dec49a2fd112381c4a40b2c8cd
MinJae Kim <skwze@naver.com>
2024-12-19 18:18:49 +0900

    KMJ 협회 메인 화면 문구 반응형 처리(2024-12-18)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -23,7 +23,7 @@ function MainLayout({children}: { children: React.ReactNode }) {
                 {/* 비디오에 텍스트 오버레이 추가 */}
                 <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
                     {/* 텍스트와 배경 처리 */}
-                    <div className="text-white text-center text-3xl md:text-5xl lg:text-7xl xl:text-8xl font-bold leading-8 p-6 bg-gray-700 bg-opacity-30 rounded-md">
+                    <div className="text-white text-center text-2xl md:text-5xl lg:text-7xl xl:text-8xl font-bold leading-8 p-6 bg-gray-700 bg-opacity-30 rounded-md">
                         <p>자신만의 특별한 작품을</p>
                         <p>BLAA에서 인증받아보세요</p>
                     </div>
